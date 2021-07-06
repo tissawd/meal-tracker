37e0edf214d170854dfa703389b27707887bb47b
Derek Tissaw <45717136+tissawd@users.noreply.github.com>
2021-07-06 14:41:50 -0500

    created dropdown component

---
+++ client/src/ui/Dropdown.js
@@ -0,0 +1,12 @@
+
+
+export const Dropdown = ({ options = [], ...props }) => (
+  <select {...props}>
+    {options.map(opt => (
+      <option
+        key={opt}
+        value={opt}
+        >{opt}</option>
+    ))}
+  </select>
+)
